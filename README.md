4193689781ae1f28f6797de0d70657117565cf51
kriskoz-yoga <184199183+kriskoz1356@users.noreply.github.com>
2026-09-14 11:41:15 +0200

    FFTW3 has been added

--- README.md
+++ README.md
@@ -8,6 +8,16 @@ Fourier Transform performance testing.
 * GCC,
 * FFTW
 
+## FFTW3 How to install on Ubuntu/Debian:
+```bash
+sudo apt update
+sudo apt install libfftw3-dev
+pkg-config --modversion fftw3 # Check the installed version
+
+# If you do not have pkgconf install it:
+sudo apt install pkgconf
+```
+
 ## Build
 
 ```bash
@@ -15,8 +25,22 @@ cmake -S . -B build
 cmake --build build
 ```
 
+or with CMake:
+
+```bash
+>CMake: Configure # Only for the first time
+
+>CMake: Build
+```
+
 ## Run
 
 ```bash
 ./build/fourierpk
+```
+
+or with CMake:
+
+```bash
+>CMake: Run Without Debugging
 ```
--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,7 @@
 #include "dft.h"
 #include "fft.h"
 #include <iostream>
+#include <fftw3.h>
 using namespace std;
 
 int main() {
@@ -10,15 +11,55 @@ int main() {
     vector<complex<double>> output_dft = dft.compute(input);
     vector<complex<double>> output_fft = fft.compute(input);
 
-    cout << "DFT Output:" << endl;
+    int N = input.size();
+    fftw_complex* fftw_input = (fftw_complex*)fftw_malloc(sizeof(fftw_complex) * N);
+
+    fftw_complex* fftw_output = (fftw_complex*)fftw_malloc(sizeof(fftw_complex) * N);
+
+    // Data preparation for FFTW3
+    for (int i = 0; i < N; ++i) {
+        fftw_input[i][0] = input[i].real();
+        fftw_input[i][1] = input[i].imag();
+    }
+
+    // Create FFTW3 plan
+    fftw_plan plan = fftw_plan_dft_1d(
+        N,
+        fftw_input,
+        fftw_output,
+        FFTW_FORWARD,
+        FFTW_ESTIMATE
+    );
+
+    // Run FFTW3
+    fftw_execute(plan);
+
+
+
+    cout << "\nDFT Output:" << endl;
     for (const auto& val : output_dft) {
         cout << val << endl;
     }
 
-    cout << "FFT Output:" << endl;
+    cout << "\nFFT Output:" << endl;
     for (const auto& val : output_fft) {
         cout << val << endl;
     }
+
+    cout << "\nFFTW3 Output:" << endl;
+    for (int i = 0; i < N; ++i) {
+        cout << "("
+             << fftw_output[i][0]
+             << ", "
+             << fftw_output[i][1]
+             << ")"
+             << endl;
+    }
+
+    // Cleanup 
+    fftw_destroy_plan(plan);
+    fftw_free(fftw_input);
+    fftw_free(fftw_output);
 
     return 0;
 }
